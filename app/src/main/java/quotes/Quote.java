d3f3d021550e6a622cf8156a084b3100387cbe1a
Lolunts <armasconi@gmail.com>
2022-11-02 14:49:58 -0700

    st

---
+++ app/src/main/java/quotes/Quote.java
@@ -0,0 +1,13 @@
+package quotes;
+
+public class Quote {
+    public Quote(String text, String author) {
+        this.text = text;
+        this.author = author;
+    }
+
+    public String text;
+    public String author;
+
+
+}
